6c17118b8ad1b7eb55f7d22af7cb03739e388550
AminDenizer <a.m.denizer@gmail.com>
2024-12-15 05:36:17 +0330

    Config Member.cs

---
+++ LibraryManagement/Member.cs
@@ -0,0 +1,11 @@
+﻿public class Member
+{
+    public string Name { get; set; }
+    public string MemberId { get; set; }
+
+    public Member(string? name, string? memberId)  // تغییر `string` به `string?`
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name), "Name cannot be null");
+        MemberId = memberId ?? throw new ArgumentNullException(nameof(memberId), "Member ID cannot be null");
+    }
+}
