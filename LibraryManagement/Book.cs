86d43500529f139aa16c763d3d63e6ef42be43ac
AminDenizer <a.m.denizer@gmail.com>
2024-12-15 05:35:13 +0330

    Config Book.cs

---
+++ LibraryManagement/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public enum Genre
+{
+    Fiction,
+    NonFiction,
+    Fantasy,
+    ScienceFiction,
+    Mystery
+}
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public Genre Genre { get; set; }
+
+    public Book(string? title, string? author, Genre genre)  // تغییر `string` به `string?`
+    {
+        Title = title ?? throw new ArgumentNullException(nameof(title), "Title cannot be null");
+        Author = author ?? throw new ArgumentNullException(nameof(author), "Author cannot be null");
+        Genre = genre;
+    }
+}
