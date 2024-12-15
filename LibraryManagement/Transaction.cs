12b56db92d1de13782b1e3367a763847c8e1adce
AminDenizer <a.m.denizer@gmail.com>
2024-12-15 05:37:37 +0330

    Config Transaction.cs

---
+++ LibraryManagement/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Transaction
+{
+    public string BookTitle { get; set; }
+    public string MemberId { get; set; }
+    public DateTime Date { get; set; }
+
+    // سازنده تراکنش
+    public Transaction(string bookTitle, string memberId, DateTime date)
+    {
+        BookTitle = bookTitle ?? throw new ArgumentNullException(nameof(bookTitle));
+        MemberId = memberId ?? throw new ArgumentNullException(nameof(memberId));
+        Date = date;
+    }
+}
